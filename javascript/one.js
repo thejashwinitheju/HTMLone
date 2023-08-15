d349cd672feda94951a254e3bb40193282af59b2
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-15 08:40:26 +0530

    output

---
+++ javascript/one.js
@@ -0,0 +1,11 @@
+/*var name = "cadbury";
+var emp = {name:"cadbury",sal:"500000"};
+var loc =["banglore", "aranakatte"];
+
+console.log(emp);   
+console.log(loc);
+console.log(name)   ;  */
+
+var x = 10;
+var y = 20;
+console.log(x+y);
