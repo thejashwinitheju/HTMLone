1bf6921a295ed31feedcb2cf09c1c36206219deb
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-16 12:46:10 +0530

    concatmenthod

---
+++ array method/concat.js
@@ -0,0 +1,8 @@
+// it marges the element
+
+var concat_animal=["dog","monkey"]
+var concat_color=["black","white"]
+var concat_name=["theju","gowri","dev","krrupa"]
+var result = concat_animal.concat(concat_color,concat_name)
+console.log(result)
+
---
+++ array method/unshift.js
@@ -0,0 +1,5 @@
+//it add one or more element in the starting array
+
+var unshift_color=["white","red","green"];
+unshift_color.unshift("black");
+console.log(unshift_color)
