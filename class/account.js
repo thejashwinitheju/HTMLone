20aca2167f31031b078e037e3cc9a565dc996ff5
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-25 19:22:25 +0530

    class

---
+++ class/account.js
@@ -0,0 +1,14 @@
+// class is templet to construct an object 
+
+
+class Account{
+    min_bal=500;
+    open_account(){
+        console.log("account oppend")
+    }
+    
+
+}
+let a1=new Account();
+console.log(a1.min_bal)
+a1.open_account();
