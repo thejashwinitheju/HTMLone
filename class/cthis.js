383d9fbd70368fa74d2dac5eb574be12377f7368
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-26 12:13:34 +0530

    this

---
+++ class/cthis.js
@@ -0,0 +1,13 @@
+class acc{
+    min_bal=1000;
+    acc_bal=25000;
+    account_details(){
+        console.log("your bank account balance :" ,this.acc_bal)
+    }
+
+
+}
+let c1 = new acc();
+console.log(c1.min_bal)
+console.log(c1.acc_bal)
+c1.account_details();
