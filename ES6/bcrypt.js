e67c6ebe5e45c1fc90ff73b1acd2a8d8f7be6517
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-28 16:54:51 +0530

    map

--- ES6/bcrypt.js
+++ ES6/bcrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 
 let user = { name:"thejashwini", email:"thejashwinitheju912@gmail.com", password:"@Thejahwini!25"}
 
-let salt = bcrypt.genSaltSync(10);
-let newpassword = bcrypt.hashSync(user.password,salt)
+let Salt = bcrypt.genSaltSync(10);
+let newpassword = bcrypt.hashSync(user.password,Saltalt)
 
 console.log(newpassword)
