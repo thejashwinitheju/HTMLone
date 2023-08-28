2b8edbe961c2638526f3600504fb917cc21f55b3
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-28 15:18:02 +0530

    foreach

---
+++ array support()/foreach.js
@@ -0,0 +1,6 @@
+let num = [11,12,13,14]
+
+let x = num.forEach((num)=>{
+   return num + 1
+})
+console.log(x)
---
+++ array support()/map.js
@@ -0,0 +1,7 @@
+let num =[11,12,13,14]
+
+let x = num.map((num)=>{
+  let x = num+100
+  return x
+})
+console.log(x)
