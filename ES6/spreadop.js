f22709858d297ccaeddf3bac391c5b9e9a6b592b
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-28 16:17:55 +0530

    spreadop

---
+++ ES6/spreadop.js
@@ -0,0 +1,6 @@
+let user ={id:1,name:"theju",email:"thejashwinitheju912@gmail.com"}
+
+let details={location:"aranakatte",salary:50000}
+
+let user_deatils={...user,...details}
+console.log(user_deatils)
