20a3c8d7b24e0ecf3dcf98a116b26cddd496b4b1
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-29 10:14:44 +0530

    settime

---
+++ ES6/settim.js
@@ -0,0 +1,10 @@
+//let employee =[
+   // {id:101,name:"theju"},
+  //  {id:102,name:"dev"}
+//]
+let createmployee =(id)=>{
+    setTimeout(()=>{
+        console.log(id)
+    },5000)
+}
+createmployee({id:103,name:"panda"})
