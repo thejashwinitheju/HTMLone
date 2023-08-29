24967848dad14895b327d24788d6e7bf350a52fd
thejashwinitheju <thejashwinitheju912@gmail.com>
2023-08-29 09:13:24 +0530

    destructring

--- ES6/bcrypt.js
+++ ES6/bcrypt.js
@@ -1,8 +1,8 @@
-const bcrypt = require('bcryptjs')
+//const bcrypt = require('bcryptJs')
 
 let user = { name:"thejashwini", email:"thejashwinitheju912@gmail.com", password:"@Thejahwini!25"}
 
 let Salt = bcrypt.genSaltSync(10);
-let newpassword = bcrypt.hashSync(user.password,Saltalt)
+let newpassword = bcrypt.hashSync(user.password,Salt)
 
 console.log(newpassword)
---
+++ ES6/destructring.js
@@ -0,0 +1,7 @@
+const emp = {
+   id:101,
+   name:"theju",
+   location:"ark"
+}
+const {id,name,location} = emp
+console.log(name,location)
